b28f84a5cbe11be370c9e3a255cc651da2d5ab0e
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-03-30 18:13:22 +0200

    base code

---
+++ js/pokedex.js
@@ -0,0 +1,7 @@
+var pokeApp = angular.module('pokedex', ['ngResource']);
+
+pokeApp.config(['$resourceProvider', function($resourceProvider) {
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+var pokeApiUrl = "http://pokeapi.co/"
