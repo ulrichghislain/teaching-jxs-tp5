62b8d04113a6d086235e176d97d87557e463a687
Maxime Tricoire <max.tricoire@gmail.com>
2016-09-18 14:34:41 +0200

    Update pokedex.js

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,8 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
+// With this you can inject POKEAPI url whenever you want
+pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
-
-var pokeApiUrl = "http://pokeapi.co/"
